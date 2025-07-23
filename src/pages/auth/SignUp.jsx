ba925b2763e5cfa9b6e5f4e0ada3bc9a40510dd9
munashe <munashemudoti@gmail.com>
2025-07-23 06:56:54 +0200

    about to finish sign up

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/SignUp.css";
 import Input from "../../components/input/Input";
 import Heading from "../../components/headings/Heading";
@@ -6,11 +6,41 @@ import Button from "../../components/buttons/Button";
 import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [name, setName] = useState()
+  // const [surname, setSurname] = useState()
+  // const [dob, setDob] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
   const navigate = useNavigate();
 
+
+  
+   const changedName = (event) => {
+    setName(event.target.value);
+  }
+
+  // const changedSurname = (event) => {
+  //   setSurname(event.target.value);
+  // }
+
+  //  const changedDOB = (event) => {
+  //   setDob(event.target.value);
+  // }
+
+   const changedEmail = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const changedPass = (event) => {
+    setPassword(event.target.value);
+  }
+
   const NextPage = () => {
+    const details = [name, email, password];
+    localStorage.setItem('info',  JSON.stringify(details))
     navigate("/signup_profile_role");
   };
+
 
   return (
     <div className="signup-root">
@@ -20,18 +50,18 @@ const SignUp = () => {
         aria-labelledby="signup-heading"
       >
         <Heading text="Sign Up" id="signup-heading" />
-        <Input type="text" hold="Name" autoFocus autoComplete="name" />
-        <Input type="text" hold="Surname" autoComplete="family-name" />
-        <Input type="date" hold="Date Of Birth" autoComplete="bday" />
-        <Input type="email" hold="Email" autoComplete="email" />
-        <Input type="password" hold="Password" autoComplete="new-password" />
+        <Input type="text" hold="Full name" autoFocus autoComplete="name" changed={changedName} />
+        {/* <Input type="text" hold="Surname" autoComplete="family-name"  changed={changedSurname}/> */}
+        {/* <Input type="date" hold="Date Of Birth" autoComplete="bday"  changed={changedDOB}/> */}
+        <Input type="email" hold="Email" autoComplete="email"  changed={changedEmail}/>
+        <Input type="password" hold="Password" autoComplete="new-password"  changed={changedPass}/>
         <Input
           type="password"
           hold="Confirm Password"
           autoComplete="new-password"
         />
         <p>
-          Don't have an Account? <a href="signup">Sign Up</a>
+          Have an Account? <a href="/">Login</a> / <a href="/">Forgot Password</a>
         </p>
         <Button name="Next" click={NextPage} />
       </div>
--- src/pages/auth/profileRole.jsx
+++ src/pages/auth/profileRole.jsx
@@ -6,16 +6,99 @@ import Button from "../../components/buttons/Button";
 import CustomDropdown from "../../components/input/customdropdown";
 import ImageUploadPreview from "../../components/input/ImageUploads";
 import { useNavigate } from "react-router-dom";
+import { API } from "../../services/config";
 
 const ProfileRole = () => {
   const [selectedRole, setSelectedRole] = useState("");
+  const [address, setAddress] = useState("N/A");
+  const [contact, setContact] = useState();
   const [profileImage, setProfileImage] = useState(null);
 
-  const navigate = useNavigate()
-const SignUp = () => {
+  const navigate = useNavigate();
+  const SignUp = () => {};
 
-}
+  const handleImageUpload = async (imageUri) => {
+    console.log("Image URI:", imageUri);
 
+    // Convert base64 to Blob if necessary
+    let blob;
+    if (imageUri.startsWith("data:")) {
+      const response = await fetch(imageUri);
+      blob = await response.blob();
+    } else {
+      // If it's a file URI, use it directly
+      blob = {
+        uri: imageUri,
+        name: imageUri.split("/").pop(),
+        type: `image/${imageUri.split(".").pop()}`,
+      };
+    }
+
+    const formData = new FormData();
+    formData.append("image", blob);
+
+    try {
+      const response = await fetch(`${API}/uploads`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Upload failed: ${errorText}`);
+      }
+
+      const data = await response.json();
+      return data.path;
+    } catch (error) {
+      console.log("Upload error:", error);
+      throw new Error("Failed to upload image");
+    }
+  };
+
+  const createUser = async () => {
+    let info = localStorage.getItem("info");
+    const userInfo = await JSON.parse(info);
+
+    const profile = await handleImageUpload(profileImage);
+    const newUser = {
+      name: userInfo.name,
+      email: userInfo.email,
+      password: userInfo.password,
+      phone_number: contact,
+      home_address: address,
+      role: selectedRole,
+      profile_picture: profile,
+    };
+
+    console.log("Going from page 3", newUser);
+    try {
+      const response = await fetch(`${API}/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Failed to create customer details: ${errorText}`);
+      }
+
+      console.log(response);
+    } catch (error) {
+      console.error()
+    }
+  };
+
+  const changedAddress = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const changedPhone = (event) => {
+    setContact(event.target.value);
+  };
 
   return (
     <div className="signup-root">
@@ -25,22 +108,33 @@ const SignUp = () => {
         aria-labelledby="signup-heading"
       >
         <Heading text="Sign Up" id="signup-heading" />
-        <form autoComplete="off" className="signup-form">
-            <label className="dropdown-label">Select Profile Picture</label>
-          <ImageUploadPreview onImageSelect={setProfileImage} />
-          <CustomDropdown
-            label="Select Role"
-            name="role"
-            value={selectedRole}
-            onChange={(e) => setSelectedRole(e.target.value)}
-            options={[
-              { value: "farmer", label: "Farmer" },
-              { value: "agronomist", label: "Agronomist" },
+        <label className="dropdown-label">Select Profile Picture</label>
+        <ImageUploadPreview onImageSelect={setProfileImage} />
+        <CustomDropdown
+          label="Select Role"
+          name="role"
+          value={selectedRole}
+          onChange={(e) => setSelectedRole(e.target.value)}
+          options={[
+            { value: "farmer", label: "Farmer" },
+            { value: "agronomist", label: "Agronomist" },
             //   { value: "admin", label: "Admin" },
-            ]}
-          />
-          <Button name="Sign Up" click={SignUp} />
-        </form>
+          ]}
+        />
+        <Input
+          type="text"
+          hold="Home Address"
+          autoComplete="Home Address"
+          changed={changedAddress}
+        />
+        <Input
+          type="phone"
+          hold="Contact"
+          autoComplete="Contact"
+          changed={changedPhone}
+        />
+
+        <Button name="Sign Up" click={SignUp} />
       </div>
     </div>
   );

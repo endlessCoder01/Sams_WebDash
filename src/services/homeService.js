dae7977a8465dea67dba288d3a14f7b3d85c124d
munashe <munashemudoti@gmail.com>
2025-08-01 20:16:18 +0200

    home service check

--- src/services/homeService.js
+++ src/services/homeService.js
@@ -5,15 +5,21 @@ export const fetchHomeData = async (userId, token) => {
       "Content-Type": "application/json",
     };
 
-    const [alertsRes, farmsRes] = await Promise.all([
+    const [alertsRes, farmsRes, tasksRes] = await Promise.all([
       fetch("http://localhost:3000/alert/with_info", { headers }),
       fetch(`http://localhost:3000/farms/user/${userId}`, { headers }),
+      fetch("http://localhost:3000/task", { headers }),
     ]);
 
     const alertsData = await alertsRes.json();
     const farmsData = await farmsRes.json();
+    const tasksData = await tasksRes.json();
 
-    return { alerts: alertsData, farms: farmsData };
+    const myTasks = tasksData.filter(
+      (task) => task.assigned_to === userId
+    );
+
+    return { alerts: alertsData, farms: farmsData, myTasks };
   } catch (err) {
     console.error("Error fetching home data:", err);
     throw err;

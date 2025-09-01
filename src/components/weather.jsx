9a1ef9d45192265b844c38c206ebb92a4b3f7ebd
munashe <munashemudoti@gmail.com>
2025-09-01 23:46:13 +0200

    all good

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -16,8 +16,6 @@ import {
   WiThunderstorm,
 } from "react-icons/wi";
 
-const API_KEY = "d0b180f6a8fba3e0262b3e23937ce9ce";
-const CITY = "Harare"; // or any city you want
 
 const WeatherCard = () => {
   const [weatherData, setWeatherData] = useState(null);
---
+++ src/services/api/weather.js
@@ -0,0 +1,2 @@
+export const API_KEY = "d0b180f6a8fba3e0262b3e23937ce9ce";
+export const CITY = "Harare"; 
